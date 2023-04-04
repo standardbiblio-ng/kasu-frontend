ba2b1f21f1c79746c67d8f541a8b5702ffd29b0d
salis sadiq <salissadiq@gmail.com>
2023-04-04 10:40:56 +0100

    Add base URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 // Backend api url
 export const backendApiURL =
-  process.env.NEXT_PUBLIC_BACKEND_API_URL || "http://localhost:5000/api/";
+  process.env.NEXT_PUBLIC_BACKEND_API_URL || "http://65.0.86.110/api/";
 
 export const userStoreName = "KASU_USER_STORE";
 
