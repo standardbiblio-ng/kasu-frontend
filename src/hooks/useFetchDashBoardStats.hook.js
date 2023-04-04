27bc8eb92d50a3607eb64a9adfbed9cc86382d07
salis sadiq <salissadiq@gmail.com>
2023-04-04 11:05:31 +0100

    deploy setup

--- src/hooks/useFetchDashBoardStats.hook.js
+++ src/hooks/useFetchDashBoardStats.hook.js
@@ -2,21 +2,18 @@ import { useQuery } from "react-query";
 import { fetchDashboardStats } from "@services/query/fetchDashboard.service";
 import { useUserStore } from "@store/userStore.store";
 import { fetchApplicants } from "@services/query/fetchApplicants.service";
-import { shallow } from 'zustand/shallow'
 
 export const useFetchDashboardStats = () => {
-    const { isAuthenticated } = useUserStore(
-        (state) => ({ isAuthenticated: state.isAuthenticated, }),
-        shallow
+    
     );
-    const { data, isLoading, isFetching, isError, isSuccess } = useQuery(
-        'dashbaordStats',
-        fetchDashboardStats,
-        {
-            enabled: isAuthenticated,
-            retry: 1
-        }
-    )
+const { data, isLoading, isFetching, isError, isSuccess } = useQuery(
+    'dashbaordStats',
+    fetchDashboardStats,
+    {
+        enabled: true,
+        retry: 1
+    }
+)
 
-    return { data, isLoading, isFetching, isError, isSuccess }
+return { data, isLoading, isFetching, isError, isSuccess }
 }
